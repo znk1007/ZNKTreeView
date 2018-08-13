ce4a9ec99f54c42a81bfc710ca04116b2b4f9290
znk1007 <1032479745@qq.com>
2018-08-13 22:53:15 +0800

    0.0.1

--- ZNKTreeView/TreeView/TreeNodeController.swift
+++ ZNKTreeView/TreeView/TreeNodeController.swift
@@ -13,7 +13,6 @@ final class TreeNodeController {
     /// 数据源代理
     var dataSource: TreeNodeControllerDataSource?
 
-
     /// 树形图数据源
     var rootNodes: [TreeNode] = []
 
@@ -26,10 +25,20 @@ final class TreeNodeController {
         if rootNodes.count == 0 || rootNodes.count != rootNumber {
             for i in 0 ..< numberOfRoot() {
                 if let node = dataSource?.treeNode(at: 0, of: nil, in: i) {
-
+                    node.indexPath = IndexPath.init(row: 0, section: i)
                 }
             }
         }
+    }
+
+    /// 插入指定节点的子节点
+    ///
+    /// - Parameters:
+    ///   - node: 指定节点
+    ///   - rootIndex: 根节点下标
+    ///   - childIndex: 子节点
+    private func insertNode(of node: TreeNode?, in rootIndex: Int, childIndex: inout Int) {
+
     }
 
     /// 根节点数
@@ -37,6 +46,16 @@ final class TreeNodeController {
     /// - Returns: 根节点数
     private func numberOfRoot() -> Int {
         return dataSource?.numberOfRootNode ?? 0
+    }
+
+    /// 指定节点的子节点数
+    ///
+    /// - Parameters:
+    ///   - node: 指定节点
+    ///   - rootIndex: 根节点下标
+    /// - Returns: 子节点数
+    private func numberOfChildNode(for node: TreeNode, in rootIndex: Int) -> Int {
+        return dataSource?.numberOfChildren(for: node, in: rootIndex) ?? 0
     }
 
 }
